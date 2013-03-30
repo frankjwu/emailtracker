b6f0630b974538bf35e23e8c1dc269d5a213c3b3
Frank Wu <fwu2012@gmail.com>
2013-03-30 02:54:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emailtracker::Application.config.secret_token = '44c6f3b1663032cb70edb02370209c0827233920d5f0b1f7697250d1a7a8f8ae459af30b0acfad3778e7050ab210aef90df05fb98f496a79eaa98b8a89c50f65'
